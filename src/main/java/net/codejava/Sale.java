20d08d9a4acdf03a74f3d656eb7375d57edcade5
Riaad Jahid <62761471+Jahid-Riaad@users.noreply.github.com>
2021-09-23 10:22:32 +0600

    m

---
+++ src/main/java/net/codejava/Sale.java
@@ -0,0 +1,79 @@
+package net.codejava;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+
+public class Sale {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String item_name;
+	private int quantity;
+	private float amount;
+
+	protected Sale() {
+	}
+
+
+
+	protected Sale(String item_name, int quantity, float amount) {
+		this.item_name = item_name;
+		this.quantity = quantity;
+		this.amount = amount;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getItem_name() {
+		return item_name;
+	}
+
+	public void setItem_name(String item_name) {
+		this.item_name = item_name;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public float getAmount() {
+		return amount;
+	}
+
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+
+	@Override
+	public String toString() {
+		return "Sale [id=" + id + ", item_name=" + item_name + ", quantity=" + quantity + ", amount=" + amount + "]";
+	}
+
+}
